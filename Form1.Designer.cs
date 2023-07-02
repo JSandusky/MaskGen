59b5a258b1d2fcc78136d94792fed3e75b3a8e69
Jonathan Sandusky <jonathan@jsandusky.com>
2023-07-01 21:58:30 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace MaskGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPickMask = new System.Windows.Forms.Button();
+            this.btnPickImage = new System.Windows.Forms.Button();
+            this.lblMask = new System.Windows.Forms.Label();
+            this.lblImage = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.progBar = new System.Windows.Forms.ProgressBar();
+            this.worker = new System.ComponentModel.BackgroundWorker();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPickMask
+            // 
+            this.btnPickMask.Location = new System.Drawing.Point(13, 13);
+            this.btnPickMask.Name = "btnPickMask";
+            this.btnPickMask.Size = new System.Drawing.Size(90, 23);
+            this.btnPickMask.TabIndex = 0;
+            this.btnPickMask.Text = "Select Mask";
+            this.btnPickMask.UseVisualStyleBackColor = true;
+            this.btnPickMask.Click += new System.EventHandler(this.btnPickMask_Click);
+            // 
+            // btnPickImage
+            // 
+            this.btnPickImage.Location = new System.Drawing.Point(13, 38);
+            this.btnPickImage.Name = "btnPickImage";
+            this.btnPickImage.Size = new System.Drawing.Size(90, 23);
+            this.btnPickImage.TabIndex = 1;
+            this.btnPickImage.Text = "Select Image";
+            this.btnPickImage.UseVisualStyleBackColor = true;
+            this.btnPickImage.Click += new System.EventHandler(this.btnPickImage_Click);
+            // 
+            // lblMask
+            // 
+            this.lblMask.AutoSize = true;
+            this.lblMask.Location = new System.Drawing.Point(109, 18);
+            this.lblMask.Name = "lblMask";
+            this.lblMask.Size = new System.Drawing.Size(0, 13);
+            this.lblMask.TabIndex = 2;
+            // 
+            // lblImage
+            // 
+            this.lblImage.AutoSize = true;
+            this.lblImage.Location = new System.Drawing.Point(109, 43);
+            this.lblImage.Name = "lblImage";
+            this.lblImage.Size = new System.Drawing.Size(0, 13);
+            this.lblImage.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::MaskGen.Properties.Resources.trans_tile;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(423, 318);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(361, 418);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // progBar
+            // 
+            this.progBar.Location = new System.Drawing.Point(13, 65);
+            this.progBar.Name = "progBar";
+            this.progBar.Size = new System.Drawing.Size(423, 23);
+            this.progBar.TabIndex = 6;
+            // 
+            // worker
+            // 
+            this.worker.WorkerReportsProgress = true;
+            this.worker.WorkerSupportsCancellation = true;
+            this.worker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.worker_DoWork);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(13, 417);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Instructions";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 447);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.progBar);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblImage);
+            this.Controls.Add(this.lblMask);
+            this.Controls.Add(this.btnPickImage);
+            this.Controls.Add(this.btnPickMask);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Mask Gen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPickMask;
+        private System.Windows.Forms.Button btnPickImage;
+        private System.Windows.Forms.Label lblMask;
+        private System.Windows.Forms.Label lblImage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ProgressBar progBar;
+        private System.ComponentModel.BackgroundWorker worker;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaskGen
+{
+    public partial class Form1 : Form
+    {
+        string maskFileName;
+        string imageFileName;
+        System.Drawing.Bitmap mask;
+        System.Drawing.Bitmap target;
+        System.Drawing.Bitmap outImg;
+        int boxW=0;
+        int boxH=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            boxW = pictureBox1.Width;
+            boxH = pictureBox1.Height;
+
+            worker.ProgressChanged += Worker_ProgressChanged;
+        }
+
+        private void btnPickMask_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Select mask image...";
+            dlg.Filter = "PNG file (*.png)|*.png";
+            dlg.CheckFileExists = true;
+            dlg.CheckPathExists = true;
+            
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                maskFileName = dlg.FileName;
+                mask = null;
+                DoWork();
+            }
+            else
+            { 
+                maskFileName = null;
+                lblMask.Text = "";
+            }
+        }
+
+        private void btnPickImage_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Select target image...";
+            dlg.Filter = "PNG file (*.png)|*.png";
+            dlg.CheckFileExists = true;
+            dlg.CheckPathExists = true;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                imageFileName = dlg.FileName;
+                target = null;
+                DoWork();
+            }
+            else
+            { 
+                imageFileName = null;
+                lblImage.Text = "";
+            }
+        }
+
+        void DoWork()
+        {
+            if (!string.IsNullOrEmpty(imageFileName))
+            {
+                lblImage.Text = imageFileName;
+
+                if (target == null)
+                    target = new System.Drawing.Bitmap(imageFileName);
+            }
+
+            if (!string.IsNullOrEmpty(maskFileName))
+            {
+                lblMask.Text = maskFileName;
+
+                if (mask == null)
+                    mask = new System.Drawing.Bitmap(maskFileName);
+            }
+
+            if (target != null && mask != null)
+            {
+                if (target.Width != mask.Width || target.Height != mask.Height)
+                {
+                    System.Windows.Forms.MessageBox.Show("Images must be the same dimensions to mask", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                outImg = new Bitmap(target.Width, target.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+                progBar.Maximum = target.Width * target.Height;
+                progBar.Step = 1;
+                progBar.Value = 0;
+                worker.CancelAsync();
+                worker.RunWorkerAsync();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (outImg != null)
+            {
+                System.Windows.Forms.SaveFileDialog dlg = new System.Windows.Forms.SaveFileDialog();
+                dlg.AddExtension = true;
+                dlg.Filter = "PNG file (*png)|*.png";
+                dlg.Title = "Save image as...";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                    outImg.Save(dlg.FileName, System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int y = 0; y < target.Height; ++y)
+            {
+                for (int x = 0; x < target.Width; ++x)
+                {
+                    Color m = mask.GetPixel(x, y);
+                    Color src = target.GetPixel(x, y);
+
+                    float frac = ((float)m.R) / 255.0f;
+                    outImg.SetPixel(x, y, Color.FromArgb((byte)(src.A * frac), (byte)(src.R * frac), (byte)(src.G * frac), (byte)(src.B * frac)));
+                }
+
+                worker.ReportProgress(target.Width * y);
+            }
+
+            worker.ReportProgress(target.Width * target.Height);
+
+            pictureBox1.Image = outImg;
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progBar.Value = e.ProgressPercentage;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            progBar.Value = 0;
+            lblImage.Text = "";
+            lblMask.Text = "";
+            pictureBox1.Image = null;
+            mask = null;
+            target = null;
+            outImg = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Load a BLACK & WHITE (or GRAYSCALE) image into the mask slot.\n\nLoad the image you want to mask into image slot.\n\nMasked image will be generated over time as the progess-bar updates. Once complete it will be displayed for preview.\n\nPress Save to save the generated image.", "Instructions", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
